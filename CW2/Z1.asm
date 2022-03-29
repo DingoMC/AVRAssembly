5a7af4683cbe45af7481f97e6a84a8e31930978e
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-03-29 20:08:52 +0200

    Update Z1.asm

--- CW2/Z1.asm
+++ CW2/Z1.asm
@@ -20,7 +20,7 @@ main:					; Kod główny programu (setup)
 loop:					; Kod wykonywany cały czas (loop)
 	; Rejestr PIN przechowuje stany z zewnątrz (wejścia)
 	SBIS PINA, 1		; Pomiń następną instrukcję jeśli bit 1 w PINA jest ustawiony (Przycisk wciśnięty)
-	CBI PORTA, 0		; Wyzeruj bit 0 na PORTA (Dioda LED nie zapali się) -> Wykona się jeśli nie spełni się warunek powyżej (coś jakby else)
+	SBI PORTA, 0		; Ustaw bit 0 na PORTA (Dioda LED zapali się) -> Wykona się jeśli nie spełni się warunek powyżej (coś jakby else)
 	SBIC PINA, 1		; Pomiń następną instrukcję jeśli bit 1 w PINA jest wyzerowany (Przycisk wyłączony)
-	SBI PORTA, 0		; Ustaw bit 0 na PORTA (Dioda LED zapali się) -> Wykona się jak przycisk będzie włączony
+	CBI PORTA, 0		; Wyzeruj bit 0 na PORTA (Dioda LED nie zapali się) -> Wykona się jak przycisk będzie włączony
 	rjmp loop			; wróć na początek
