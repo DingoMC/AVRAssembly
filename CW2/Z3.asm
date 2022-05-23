db9518cb8b0317bfba6118b4d54af5db4bdc2c74
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-05-23 19:09:36 +0200

    Update Z3.asm

--- CW2/Z3.asm
+++ CW2/Z3.asm
@@ -1,61 +1,63 @@
-//Poniżej podaj swoje dane
-//<Imię i nazwisko>
-//<Nr indeksu>
-//<Grupa studencka>
-//<Grupa laboratoryjna>
-//Cw2Z3
 ///////////////////////
+// PA0->D0
+// PB0->D1
+// PC0->D2
+// PD0->D3
+// PA7->W1
+// PD7->W2
+// K4->GND
 
-.include "m32def.inc"	; Jakaś tam biblioteka
-.org 0x00				; coś tam z pamięcią (nieważne pisz)
-jmp main				; Skocz do maina
-.org 0x100				; to też coś z pamięcią (też pisz)
-main:					; Kod główny programu (setup)
-	; Ustawienie pinów dla LED na wyjścia
+.include "m32def.inc"
+	
+	.org 0x00
+	jmp main
+
+	.org 0x100
+main: 			; program główny
+				; tutaj ciąg instrukcji inicjalizacyjnych
+	; Ustawianie PA0, PB0, PC0, PD0 jako wyjścia
 	SBI DDRA, 0
 	SBI DDRB, 0
 	SBI DDRC, 0
 	SBI DDRD, 0
-	; Ustawienie pinów dla przycisków na wejścia
-	CBI DDRA, 7			; Przycisk A
-	SBI PORTA, 7
-	CBI DDRD, 7			; Przycisk B
-	SBI PORTD, 7
-loop:					; Kod wykonywany cały czas (loop)
-	; Sekwencja a
+	CBI DDRA, 7		; Bit 7 portu A jako wejście (przycisk "A")
+	SBI PORTA, 7	; Podciągnięcie PA7 do zasilania
+	CBI DDRD, 7		; Bit 7 portu D jako wejście (przycisk "B")
+	SBI PORTD, 7	; Podciągnięcie PD7 do zasilania
+loop:			; początek pętli głównej
+				; instrukcje pętli głównej
+	; Sekwencja a (ON|ON|OFF|OFF)
 	SBI PORTA, 0
 	SBI PORTB, 0
 	CBI PORTC, 0
 	CBI PORTD, 0
-	SBIC PIND, 7		; Jeżeli przycisk B nie jest wciśnięty pomiń następną instrukcję
-	SBI PORTD, 0		; Jeśli jest wciśnięty to włącz diodę D
+	SBIS PIND, 7	; Jeżeli przycisk B nie będzie wciśnięty to pomiń następną instrukcję
+	SBI PORTD, 0	; Jeżeli przycisk B wciśnięty to zaświeć diodę D3
 	; Opóźnienie 500ms
-	ldi  r18, 3
-    ldi  r19, 138
-    ldi  r20, 86
+	ldi  r18, 41
+    ldi  r19, 150
+    ldi  r20, 128
 L1: dec  r20
     brne L1
     dec  r19
     brne L1
     dec  r18
     brne L1
-    rjmp PC+1
-	; Sekwencja b
+	; Sekwencja b (OFF|OFF|ON|ON)
 	CBI PORTA, 0
-	SBIC PINA, 7		; Jeżeli przycisk A nie jest wciśnięty pomiń następną instrukcję
-	SBI PORTA, 0		; Jeśli jest wciśnięty to włącz diodę A
+	SBIS PINA, 7	; Jeżeli przycisk A nie będzie wciśnięty to pomiń następną instrukcję
+	SBI PORTA, 0	; Jeżeli przycisk A wciśnięty to zaświeć diodę D0
 	CBI PORTB, 0
 	SBI PORTC, 0
 	SBI PORTD, 0
 	; Opóźnienie 200ms
-	ldi  r18, 2
-    ldi  r19, 4
-    ldi  r20, 187
+	ldi  r18, 17
+    ldi  r19, 60
+    ldi  r20, 204
 L2: dec  r20
     brne L2
     dec  r19
     brne L2
     dec  r18
     brne L2
-    nop
-	rjmp loop			; Wróć na początek
+	rjmp loop 	; wróć na początek pętli głównej
