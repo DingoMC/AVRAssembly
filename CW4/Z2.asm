867e4c461388e40525f80f00a72c0ccbc48c772f
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-06-15 11:43:43 +0200

    Update Z2.asm

--- CW4/Z2.asm
+++ CW4/Z2.asm
@@ -1,19 +1,10 @@
 ///////////////////////
-// a -> PC0
-// b -> PC1
-// c -> PC2
-// d -> PC3
-// e -> PC4
-// f -> PC5
-// g -> PC6
-// dp -> PC7
+// a-g,dp -> PC0-7
 // GND -> C4
-
 // D0 -> PA0
-
-// K1 -> PD2
+// K3 -> PD2
 // K4 -> PD3
-// GND -> W1
+// GND -> W4
 
 
 .include "m32def.inc"
@@ -26,15 +17,11 @@
 	jmp prz1	; przerwanie INT1
 main: 			; program główny
 	CLI
-	LDI R16, high(RAMEND) ; inicjalizacja wskaźnika stosu
-	OUT SPL, R16
-	LDI R16, low(RAMEND)
-	OUT SPH, R16
 	LDI R16, 0xC0		; Obsługa przerwań
 	OUT GICR, R16
 	SBI PORTD, 3		; przycisk INT1
 	SBI PORTD, 2		; przycisk INT0
-	LDI R16, 0x02
+	LDI R16, 0x0A		; Reakcja przerwań tylko na zbocze opadające
     OUT MCUCR, R16		; Rejestr sterujący przerwaniami
 	LDI R16, 0xFF
     OUT DDRA, R16
@@ -42,12 +29,12 @@ main: 			; program główny
 	SBI PORTC, 7		; Zapalenie wyswietlacza
 	CBI PORTA, 0		; Dioda na początku wyłączona
 	SEI					; Włączenie obsługi przerwań
-loop:			; początek pętli głównej
+loop:				; początek pętli głównej
 	IN R16, PORTC
     ROR R16
     OUT PORTC, R16	; Pobranie wartości z wyświetlacza i wysłanie nowej
 	CALL delay
-	rjmp loop 	; wróć na początek pętli głównej
+	rjmp loop 		; wróć na początek pętli głównej
 delay:
     ldi  r18, 26	; Opóźnienie 20ms
     ldi  r19, 249
@@ -57,22 +44,14 @@ L1: dec  r19
     brne L1
     RET
 prz0:
-	PUSH R16
-    IN R16, SREG
-    PUSH R16
-	SBI PORTA, 0		; Zapalenie diody po wciśnięciu 
-    POP R16
-    OUT SREG, R16
-    POP R16
+	SBI PORTA, 0		; Zapalenie diody po wciśnięciu (SBI nie narusza SREG zatem nie trzeba korzystać ze stosu)
 	RETI
 prz1:
-	PUSH R16
-    IN R16, SREG
-    PUSH R16
-    IN R16, PORTA
-    COM R16				; Zmiana stanu diody na przeciwny
-    OUT PORTA, R16
-    POP R16
-    OUT SREG, R16
-    POP R16
-	RETI
+	SBIS PORTA, 0				; Jeżeli D0 jest wyłączona to skocz do instrukcji włączenia
+	RJMP LED_on
+	SBIC PORTA, 0				; Jeżeli D0 jest włączona wyłącz
+	CBI PORTA, 0
+	RETI						; Powrót z przerwania
+LED_on:							; Instrukcja właczenia D0
+	SBI PORTA, 0
+	RETI						; Powrót z przerwania
